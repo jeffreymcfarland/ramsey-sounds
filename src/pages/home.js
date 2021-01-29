5b7c3b17e2081f857ac8591b6d31d4b5c5a85bec
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2021-01-29 17:00:49 -0500

    separate sass styles from pages

---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import "../assets/sass/home.scss";
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <div class='headingWrapper'>
+          <h1 class='heading'>Ramsey Sounds</h1>
+        </div>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
